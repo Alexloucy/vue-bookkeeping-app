a6446c0b1a254ff2d58b9e67e7e5157767265202
alexloucy <alexloucy@icloud.com>
2023-04-15 17:49:26 +1200

    Empty Home page finished

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <h1 v-if="isEmpty">
+      Start by clicking the plus icon on the top right corner
+    </h1>
+    <div id="plus_out" @click="createJournal()"><div id="plus">+</div></div>
+  </div>
 </template>
 
 <script>
@@ -11,5 +16,40 @@ export default {
   components: {
     HelloWorld,
   },
+  data() {
+    return {
+      isEmpty: true,
+      journalList: [],
+      journal: {},
+    };
+  },
+  methods: {
+    createJournal() {
+      this.isEmpty = !this.isEmpty;
+    },
+  },
 };
 </script>
+
+<style>
+#plus_out {
+  border-style: solid;
+  border-color: blue;
+  border-radius: 0.5ch;
+  position: absolute;
+  top: 55px;
+  right: 16px;
+  width: 25px;
+  height: 25px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+}
+#plus {
+  color: blue;
+  vertical-align: middle;
+  font-size: large;
+  font-weight: bold;
+}
+</style>
